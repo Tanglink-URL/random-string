d4cc30de000cbcb6169ea7c8e510cf3c8254bbdd
Cody Ko In A Box <codykoinabox@gmail.com>
2023-06-22 15:10:13 -0300

    Create rand.js

---
+++ rand.js
@@ -0,0 +1,22 @@
+// EXAMPLE SET
+let exampleSet = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz123456789"
+
+function randString(length, set) {
+
+    let output = ''
+
+    const setLength = set.length
+
+    for(let i = 0; i < length; i++){
+        output += set.charAt(Math.floor(Math.random()*setLength))
+    }
+
+    return output
+}
+
+// EXAMPLE OUTPUTS FOR randString(5, exampleSet)
+// pWJko
+// 5HMLG
+// 6EV79
+// TYRke
+// re4kF
